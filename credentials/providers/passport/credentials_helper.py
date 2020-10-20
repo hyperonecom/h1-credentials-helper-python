4210ad6f59a80722036bf4f35d20d9fd5b8f800d
kkm <kubasurdej@gmail.com>
2020-10-20 22:08:14 +0200

    Add function for getting credentials helper

---
+++ credentials/providers/passport/credentials_helper.py
@@ -0,0 +1,8 @@
+from ...auth.jwt.rsa_signer import RSASigner
+from .file_loader import load_passport_file
+
+
+def get_credentials_helper(location):
+    passport_data = load_passport_file(location)
+    return RSASigner(passport_data['private_key'], passport_data['certificate_id'], passport_data['issuer'],
+                     passport_data['subject_id'], 'RSA256')
