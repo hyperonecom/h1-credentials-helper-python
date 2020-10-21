0247ee992cc52a5f0e3367c3615f2b12d66b8d64
kkm <kubasurdej@gmail.com>
2020-10-21 18:54:44 +0200

    Create test for getting default passport location

--- test/providers/passport/file_loader_test.py
+++ test/providers/passport/file_loader_test.py
@@ -1,9 +1,11 @@
-from credentials.providers.passport.file_loader import load_passport_file
+from credentials.providers.passport.file_loader import load_passport_file, get_default_passport_location
 from credentials.providers.passport.exceptions import InvalidPassportException
 import unittest
+from unittest.mock import patch
 import pathlib
 from os import path
 from .fixture_helper import get_fixture_location
+from pathlib import WindowsPath, Path
 
 
 class TestLoadingCredentialsFile(unittest.TestCase):
@@ -29,6 +31,22 @@ class TestLoadingCredentialsFile(unittest.TestCase):
 
         # too long for assert equal testing
         self.assertNotEqual(loaded_passport["private_key"], "")
+
+
+class TestPassportValidation(unittest.TestCase):
+    pass
+
+
+class TestGettingDefaultPassportLocation(unittest.TestCase):
+
+    def test_getting_passport_location(self):
+        with patch.object(Path, 'home') as mock:
+            path_object_mock = WindowsPath("C:/Users/Jakup")
+            mock.return_value = path_object_mock
+
+            passport_path = get_default_passport_location()
+            self.assertEqual(
+                passport_path, "C:\\Users\\Jakup\\.h1\\passport.json")
 
 
 if __name__ == '__main__':
