c116c8708f1bf47bf9959dba25c9c12910b1a348
kkm <kubasurdej@gmail.com>
2020-10-25 15:01:10 +0100

    Add PyJWT to requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+REQUIRES = ["PyJWT>=1.7.1"]
+
 setuptools.setup(
     name="credentials-hyperonecom",
     version="0.0.1",
@@ -11,11 +13,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kuskoman/h1-credentials-helper",
-    packages=setuptools.find_packages(),
+    install_requires=REQUIRES,
+    packages=setuptools.find_packages(exclude=["test", "tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
